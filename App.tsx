ce604c15396fe76aab9d129d653d33bfb0eb4980
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2025-05-21 08:49:00 +0530

    dark theme option added

--- App.tsx
+++ App.tsx
@@ -10,20 +10,23 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { AuthProvider } from './src/context/AuthContext';
 import { HabitProvider } from './src/context/HabitContext';
+import { ThemeProvider } from './src/context/ThemeContext';
 import { AppNavigator } from './src/navigation/AppNavigator';
 
 const App = () => {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaProvider>
-        <AuthProvider>
-          <HabitProvider>
-            <AppNavigator />
-          </HabitProvider>
-        </AuthProvider>
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
-  );
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <SafeAreaProvider>
+                <ThemeProvider>
+                    <AuthProvider>
+                        <HabitProvider>
+                            <AppNavigator />
+                        </HabitProvider>
+                    </AuthProvider>
+                </ThemeProvider>
+            </SafeAreaProvider>
+        </GestureHandlerRootView>
+    );
 };
 
 export default App;
